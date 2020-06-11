304d8ab9f212f6d6dfbaae381977050ecef46229
Ayushi Gupta <ayushigupta8@gmail.com>
2020-06-11 22:14:53 +0530

    Updated home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import styles from './Home.module.css';
 import logo from '../../assets/logo.png';
-import {ReactComponent as image} from '../../assets/movieSvg.svg';
+import image from '../../assets/movieSvg.svg';
 
 function Home() {
     return (
         <div className={styles.mainContainer}>
             <div className={styles.logoContainer}>
-                <img src={logo} className={styles.logo} alt="logo" />
+                <img src={logo} className={styles.logo} alt='logo' />
             </div>
             <div className={styles.content}>
                 <div className={styles.leftContainer}>
                     <span className={styles.title}>Cinephile.</span>
-                    <span className={styles.subtitle}>Movies. Series. And more!</span>
+                    <span className={styles.subtitle}>All about movies and series!</span>
+                    {/* <div className={styles.searchBar}> */}
+                        <input type='text' name='search' placeholder='Search your favourite movies, series...' className={styles.searchBar} />
+                    {/* </div> */}
                 </div>
                 <div className={styles.rightContainer}>
-                    <svg src={image} style={styles.image} />
+                    <img src={image} className={styles.image} alt='movie' />
                 </div>
             </div>
         </div>
